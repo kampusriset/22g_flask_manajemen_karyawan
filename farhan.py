014b6f89903d6209000ae8c212f41dc2af2129ef
farhannn96 <f4rhannuralam96@gmail.com>
2025-02-19 19:56:34 +0700

    Add files via upload

---
+++ farhan.py
@@ -0,0 +1,10 @@
+def perkenalan(nama, nim, peran):
+    print("Halo! Perkenalkan, nama saya", nama)
+    print("NIM : ", nim)
+    print("Peran saya yaitu : ", peran)
+
+if __name__ == "__main__":
+    nama = "Farhan Nur Alam"
+    nim = "2213010401"
+    peran = "Mengerjakan fitur pada login dan register"
+    perkenalan(nama, nim, peran)
