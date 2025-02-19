6ad34114e4cb85cecdd59677ccce04ed28c0c408
SigitCode <sigitdwianto78@gmail.com>
2025-02-19 17:44:57 +0700

    Add files via upload

---
+++ Team/pratama.py
@@ -0,0 +1,10 @@
+def perkenalan(nama, nim, peran):
+    print("Halo! Perkenalkan, nama saya", nama)
+    print("NIM : ", nim)
+    print("Peran saya yaitu : ", peran)
+
+if __name__ == "__main__":
+    nama = "Pratama Andika Setyawan"
+    nim = "2213010407"
+    peran = "Mengerjakan fitur pada update dan create"
+    perkenalan(nama, nim, peran)
---
+++ Team/sigit.py
@@ -0,0 +1,10 @@
+def perkenalan(nama, nim, peran):
+    print("Halo! Perkenalkan, nama saya", nama)
+    print("NIM : ", nim)
+    print("Peran saya yaitu : ", peran)
+
+if __name__ == "__main__":
+    nama = "Sigit Dwianto"
+    nim = "2213010403"
+    peran = "Mengerjakan fitur pada index dan dashboard"
+    perkenalan(nama, nim, peran)
