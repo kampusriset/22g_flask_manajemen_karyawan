b95e46fc87f339b39fedd14c2b39a41defc560ec
AlpinDanuarta <danuartaalpin@gmail.com>
2025-02-19 19:32:16 +0700

    Add files via upload

---
+++ Team/alpin.py
@@ -0,0 +1,10 @@
+def perkenalan(nama, nim, peran):
+    print("Halo! Perkenalkan, nama saya", nama)
+    print("NIM : ", nim)
+    print("Peran saya yaitu : ", peran)
+
+if __name__ == "__main__":
+    nama = "Alpin Danuarta"
+    nim = "2213010386"
+    peran = "Mengerjakan fitur pada change password dan membuat database"
+    perkenalan(nama, nim, peran)
